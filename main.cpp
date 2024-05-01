8418733d3bbe188838e509af48030006bddd9f87
bittiboop <sstrelkova15@gmail.com>
2024-05-01 19:09:11 +0300

    added ex 1

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+using namespace std;
+
+int seconds, minutes, hours;
+
+
+int main()
+{
+	srand(time(NULL));
+
+	int r = rand() % 500;
+	int num, trys = 0;
+	
+
+	chrono::steady_clock sc;
+	auto start = sc.now();
+	for (;;)
+	{
+		cout << "Enter number: ";
+		cin >> num;
+		if (num < r || num < 500)
+		{
+			cout << "Oh! Computer`s number is more than yours.\n" << endl;
+			trys++;
+
+			continue;
+		}
+		if (num > r || num < 500)
+		{
+			cout << "Oh! Computer`s number is less than yours.\n " << endl;
+			trys++;
+
+			continue;
+		}
+		else
+		{
+			auto end = sc.now();
+			auto time_span = static_cast<chrono::duration<double>>(end - start);
+			cout << "Right! It`s " << r << " your`e tries: " << trys << "\nYour`e time: " << time_span.count() << "seconds." << endl;
+			
+			break;
+		}
+	}
+
+	return 0;
+}

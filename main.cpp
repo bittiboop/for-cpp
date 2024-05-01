1af9c3e72aff9cd26680cee995e75ddf144ee084
bittiboop <sstrelkova15@gmail.com>
2024-05-01 20:44:55 +0300

    added start code

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ int main()
 {
 	srand(time(NULL));
 
-	int r = rand() % 2;
+	
+	for (size_t i = 0; i < 100; i++)
+	{
+		int r = rand() % 800 + 100;
+		cout << r << "  ";
+	}
+	cout << endl;
 
 	return 0;
 }
